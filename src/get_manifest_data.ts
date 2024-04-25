7b01a0eb6adddcb2048137d6149531e29b20ab90
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-04-25 22:43:49 +0200

    Update get_manifest_data.ts

--- src/get_manifest_data.ts
+++ src/get_manifest_data.ts
@@ -3,4 +3,3 @@ import type { Manifest } from './response_objects/manifest.ts';
 export default function getManifestData(apiEndpoint: string) : Promise<Manifest> {
   return fetch(apiEndpoint).then((response) => response.json()) as Promise<Manifest>
 }
-
